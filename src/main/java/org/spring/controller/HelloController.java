6c521b730633cdc443408357d89a4747fa880ada
tkhsksk <tkhsksk0318@gmail.com>
2024-10-07 17:33:24 +0900

    .gitignore作成

---
+++ src/main/java/org/spring/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/")
+    public String index() {
+        return "NewFile";
+    }
+}
